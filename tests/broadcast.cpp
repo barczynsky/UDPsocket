f8e6b20a9f26fa31f96f9185e3c75a7cc2c7ffcd
barczynsky <barczynsky@users.noreply.github.com>
2017-11-05 15:41:41 +0100

    minor tweaks to CMake and broadcast example

--- tests/broadcast.cpp
+++ tests/broadcast.cpp
@@ -2,18 +2,20 @@
 #include <string>
 #include <thread>
 using namespace std::literals;
+
 #include "UDPsocket.h"
 
 
-static constexpr uint16_t port = 2020;
+static constexpr uint16_t PortNo = 2020;
+static constexpr uint8_t IterCount = 20;
+static constexpr auto IterDuration = 1s;
 
 
 void test_broadcast()
 {
-
 	UDPsocket ss;
 	ss.open();
-	ss.bind(port);
+	ss.bind(PortNo);
 
 	UDPsocket cs;
 	cs.open();
@@ -21,8 +23,8 @@ void test_broadcast()
 
 	auto t1 = std::thread([&ss, &cs]
 	{
-		std::this_thread::sleep_for(1s);
-		while (true)
+		std::this_thread::sleep_for(IterDuration * 1.25);
+		for (uint16_t i = 0; i < 2 * IterCount; ++i)
 		{
 			UDPsocket::IPv4 ipaddr;
 			std::string data;
@@ -32,29 +34,34 @@ void test_broadcast()
 			}
 			else
 			{
-				fprintf(stderr, "%s  '%s'\n", ipaddr.to_string().c_str(), data.c_str());
-				if (data.compare(0, 8, "MESSAGE?"s) == 0)
+				if (!data.empty())
 				{
-					ipaddr.port = port;
-					if (cs.send("MESSAGE!"s, ipaddr) < 0)
+					fprintf(stderr, "%s  '%s'\n", ipaddr.to_string().c_str(), data.c_str());
+					if (data.compare(0, 8, "MESSAGE?"s) == 0)
 					{
-						fprintf(stderr, "send(): failed\n");
+						ipaddr.port = PortNo;
+						if ((i & 0x2 ? cs : ss).send("MESSAGE!"s, ipaddr) < 0)
+						{
+							fprintf(stderr, "send(): failed (REP)\n");
+						}
 					}
 				}
 			}
 		}
+		ss.close();
 	});
 
 	auto t2 = std::thread([&cs]
 	{
-		while (true)
+		for (uint8_t i = 0; i < IterCount; ++i)
 		{
-			if (cs.send("MESSAGE?"s, UDPsocket::IPv4::Broadcast(port)) < 0)
+			if (cs.send("MESSAGE?"s, UDPsocket::IPv4::Broadcast(PortNo)) < 0)
 			{
-				fprintf(stderr, "send(): failed\n");
+				fprintf(stderr, "send(): failed (REQ)\n");
 			}
-			std::this_thread::sleep_for(1s);
+			std::this_thread::sleep_for(IterDuration);
 		}
+		cs.close();
 	});
 
 	t1.join();
